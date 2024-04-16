b3d61bb368aea79d7b03c1665021349a6ff85653
Chablio <chabliomar82@gmail.com>
2024-04-16 10:46:06 +0200

    raycast click done

--- Assets/Mix.cs
+++ Assets/Mix.cs
@@ -21,17 +21,17 @@ public class NewBehaviourScript : MonoBehaviour
         // check for mouse click
         if (Input.GetMouseButtonDown(0))
         {
-            Ray Ray = cam.ScreenPointToRay(Input.mousePosition);
+            pos.x = Input.mousePosition.x;
+            pos.y = Input.mousePosition.y;
+            float maxDistance = 3;
+            Ray ray = cam.ScreenPointToRay(pos);
+            //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+            if(Physics.Raycast(ray, out RaycastHit hit, maxDistance))
+            {
+                Debug.Log(hit.collider.gameObject.name + " was hit");
+            }
         }
-        pos.x = Input.mousePosition.x;
-        pos.y = Input.mousePosition.y;
-        float maxDistance = 1;
-        Ray ray = cam.ScreenPointToRay(pos);
-        //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-        if(Physics.Raycast(ray, out RaycastHit hit, maxDistance))
-        {
-            Debug.Log(hit.collider.gameObject.name + " was hit");
-        }
+       
     }
 
 
