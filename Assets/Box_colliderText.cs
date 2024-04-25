c03bed4573369a77d5e1a06be96e73667b853fd0
Chablio <chabliomar82@gmail.com>
2024-04-25 20:46:46 +0200

    test

--- Assets/Box_colliderText.cs
+++ Assets/Box_colliderText.cs
@@ -1,14 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Box_colliderText : MonoBehaviour
-{
-    void OnTriggerEnter(UnityEngine.Collider other)
-    {
-        if (other.gameObject.tag == "Sphere")
-        {
-            print("ENTER!");
-        }
-    }
-}
+//public class Box_colliderText : MonoBehaviour
+//{
+//    void OnTriggerEnter(UnityEngine.Collider other)
+//    {
+//        if (other.gameObject.tag == "Sphere")
+//        {
+//            print("ENTER!");
+//        }
+//    }
+//}
