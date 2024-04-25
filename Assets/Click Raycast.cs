7b7382abf3f460c795d39494776c9ba405f6867e
Chablio <chabliomar82@gmail.com>
2024-04-25 13:44:37 +0200

    FÆRDIG

--- Assets/Click Raycast.cs
+++ Assets/Click Raycast.cs
@@ -1,91 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     Camera cam;
     Vector3 pos = new Vector3(0, 0, 0);
     public GameObject door;
-
     public GameObject UICanvas;
 
     string chemical1 = "";
     string chemical2 = "";
-    // Start is called before the first frame update
+
+    public GameObject box1;
+    public GameObject box2;
+
+    private bool box1Clicked = false;
+    private bool box2Clicked = false;
+
     void Start()
     {
         cam = GetComponent<Camera>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // check for mouse click
         if (Input.GetMouseButtonDown(0))
         {
             pos.x = Input.mousePosition.x;
             pos.y = Input.mousePosition.y;
             float maxDistance = 3;
             Ray ray = cam.ScreenPointToRay(pos);
-            //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-            if(Physics.Raycast(ray, out RaycastHit hit, maxDistance))
+
+            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
             {
                 Debug.Log(hit.collider.gameObject.name + " was hit");
-                if(hit.collider.gameObject.CompareTag("Container"))
+
+                if (hit.collider.gameObject.CompareTag("Container"))
                 {
                     Chemicalspotions mix = hit.collider.gameObject.GetComponent<Chemicalspotions>();
                     Debug.Log("It was a container with " + mix.chemical);
 
-                    if(chemical1 == "")
+                    if (chemical1 == "")
                     {
                         chemical1 = mix.chemical;
                     }
-                    else if(chemical2 == "")
+                    else if (chemical2 == "")
                     {
                         chemical2 = mix.chemical;
                     }
                 }
+                else if (hit.collider.gameObject == box1)
+                {
+                    Debug.Log("Box 1 was hit!");
+                    box1Clicked = true;
+                }
+                else if (hit.collider.gameObject == box2)
+                {
+                    Debug.Log("Box 2 was hit!");
+                    box2Clicked = true;
+                }
+
+                if (box1Clicked && box2Clicked)
+                {
+                    Debug.Log("You won!");
+                }
+            }
+
+            if ((chemical1 == "Blue" && chemical2 == "Green") || (chemical1 == "Green" && chemical2 == "Blue"))
+            {
+                Debug.Log("solved!");
+                door.SetActive(false);
+            }
+            else if ((chemical1 != "" && chemical2 != "") && ((chemical1 != "Blue" && chemical2 != "Green") || (chemical1 != "Green" && chemical2 != "Blue")))
+            {
+                Debug.Log("wrong!");
+                chemical1 = "";
+                chemical2 = "";
             }
         }
-        
-
-        if((chemical1 == "Blue" &&  chemical2 == "Green") || (chemical1 == "Green" && chemical2 == "Blue"))
-        {
-            Debug.Log("solved!");
-            door.SetActive(false);
-
-        }
-        else if((chemical1 != "" && chemical2 != "") && ((chemical1 != "Blue" && chemical2 != "Green") || (chemical1 != "Green" && chemical2 != "Blue")))
-        {
-            Debug.Log("wrong!");
-            chemical1 = "";
-            chemical2 = "";
-        }
-
-        UICanvas.SetActive(false);
-
-        UICanvas.SetActive(true);
-
-        /*if ((chemical1 == "Black" && chemical2 == "Green") || (chemical1 == "Green" && chemical2 == "Black"))
-        {
-            Debug.Log("wrong!");
-            door.SetActive(false);
-
-        }
-        else if (chemical1 != "" && chemical2 != "")
-        {
-            Debug.Log("wrong!");
-            chemical1 = "";
-            chemical2 = "";
-        }*/
-
     }
-
-
 }
+
 
 
